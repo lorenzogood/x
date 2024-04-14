0a2bcba0eefff97f1ca8e63592e0cd4899d88778
foehammer <44120040+foehammer127@users.noreply.github.com>
2024-04-14 16:51:24 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    alejandra = {
+      url = "github:kamadorueda/alejandra/3.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {self, ...}:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} (toplevel @ {withSystem, ...}: {
+      systems = ["aarch64-darwin" "aarch64-linux" "x86_64-linux"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          localSystem = system;
+          config = {
+            allowUnfree = true;
+            allowAliases = true;
+          };
+          overlays = [inputs.rust-overlay.overlays.default];
+        };
+
+        devShells = import ./nix/devshells.nix pkgs;
+
+        packages = import ./nix/package-sets.nix pkgs;
+
+        formatter = inputs'.alejandra.packages.default;
+      };
+    });
+}
---
+++ src/test/devshell.nix
@@ -0,0 +1,3 @@
+{mkShell, ...}:
+mkShell {
+}
